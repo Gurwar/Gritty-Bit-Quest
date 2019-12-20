3f87b51aad9da2e9c71ada932033f8c6361ae98e
GurWar <Gurinder.waraich@hotmail.com>
2019-12-19 21:26:25 -0500

    OVR and AI

---
+++ Gritty Bit Quest/Gritty Bit Quest/Assets/TriggerEvent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public enum TriggerType { ObjectGrabbed, TriggerWithPlayer};
+    [SerializeField]
+    TriggerType triggerType;
+    OVRGrabbable GrabbableScript;
+    [SerializeField]
+    GameObject target;
+    [SerializeField]
+    string message;
+    bool triggered;
+    void Start()
+    {
+        GrabbableScript = GetComponent<OVRGrabbable>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerType == TriggerType.ObjectGrabbed)
+        {
+            if (GrabbableScript)
+            {
+                if(GrabbableScript.isGrabbed)
+                {
+                    target.SendMessage(message, SendMessageOptions.RequireReceiver);
+                    Destroy(this);
+                }
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                target.SendMessage(message, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (triggerType == TriggerType.ObjectGrabbed)
+        {
+            if (collider.transform.root.name == "Player")
+            {
+                collider.gameObject.SendMessage(message, SendMessageOptions.RequireReceiver);
+                Destroy(this);
+            }
+        }
+    }
+}
---
+++ Gritty Bit Quest/Gritty Bit Quest/Assets/VRObjectScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRObjectScript : MonoBehaviour
+{
+    public int id;
+    bool grabbed;
+    OVRGrabbable OVR;
+
+    private void Start()
+    {
+        OVR = GetComponent<OVRGrabbable>();
+    }
+
+    void Update()
+    {
+        //get frame when item is grabbed
+        if (!grabbed)
+        {
+            if (OVR.isGrabbed)
+            {
+                grabbed = true;
+                transform.parent = OVR.grabbedBy.transform;
+            }
+        }
+        else
+        {
+            if (!OVR.isGrabbed)
+            {
+                grabbed = false;
+                transform.parent = null;
+            }
+        }
+    }
+
+}
